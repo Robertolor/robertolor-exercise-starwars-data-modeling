d6daa61145127b5cf99fef69f29767310ce3ef1d
Roberto <51332297+Robertolor@users.noreply.github.com>
2024-04-23 02:52:17 +0000

    esta listo, creo

--- src/models.py
+++ src/models.py
@@ -7,26 +7,44 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
+class User(Base):
+    __tablename__ = 'User'
+    id = Column(Integer, primary_key=True) 
     name = Column(String(250), nullable=False)
+    email= Column(String(250), nullable=False)
+    constraseña= Column(String(250), nullable=False)
+    username= Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+class Favorite(Base):
+    __tablename__ = 'Favorite'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_id=Column(Integer, ForeignKey('User.id'), nullable=False)
+    characters_id = Column(Integer, ForeignKey('Characters.id'), nullable=True)
+    planets_id = Column(Integer, ForeignKey('Planets.id'), nullable=True)
 
-    def to_dict(self):
-        return {}
+class Characters(Base):
+    __tablename__='Characters'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    description = Column(String(250), nullable=False)
+    height = Column(Integer, nullable=False)
+    weight = Column(Integer, nullable=False)
+    age = Column(Integer, nullable=False)
+
+class Planets(Base):
+    __tablename__='Planets'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    description = Column(String(250), nullable=True)
+    diameter = Column(Integer, nullable=True)
+    rotation = Column(Integer, nullable=True)
+    gravity = Column(String(250), nullable=True)
+    population = Column(Integer, nullable=True)
+    climate = Column(String(250), nullable=True)
+    terrain = Column(String(250), nullable=True)
+
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
